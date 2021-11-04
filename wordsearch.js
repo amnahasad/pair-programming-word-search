ce36dc096c0a3b1760ed4c08954fa2bee0dc9308
amnahasad <amnah_asad@hotmail.com>
2021-11-04 18:06:08 +0000

    Added the wordSearch vertical function and test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (letters.length === 0) {
+    console.log('hello');
+    return false;
+  }
+
+  if (typeof word !== 'string') {
+    console.log('hello2');
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  let newArr = [];
+
+  for (let i = 0; i < letters.length - 1; i++) {
+    let tempArr = [];
+
+    for (let j = 0; j < letters[i].length; j++) {
+      tempArr.push(letters[j][i]);
+    }
+    newArr.push(tempArr);
+  }
+  const verticalJoin = newArr.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+
+
+module.exports = wordSearch;
